ec9e7458e3549c7f8acf09b263b8c52ca6484b78
jadilovic <j.adilovic@gmail.com>
2023-04-13 09:01:29 +0200

    fixing errors and improving the solution

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const menuIcon = document.querySelector('.menu');
-const ulMenu = document.querySelector('ul');
+const ulMenu = document.querySelector('.menu-list');
 const closeIcon = document.querySelector('.close-icon');
+const overlay = document.getElementById('overlay');
 
 menuIcon.addEventListener('click', () => {
 	ulMenu.style.right = 0;
+	overlay.style.display = 'block';
 });
 
 closeIcon.addEventListener('click', () => {
 	ulMenu.style.right = '-100%';
+	overlay.style.display = 'none';
 });
